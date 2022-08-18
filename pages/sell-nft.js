1f49170358e9e724f262868ce3e24b6b42b24beb
Dav17d <75180393+Dav17d@users.noreply.github.com>
2022-08-19 00:20:28 +0500

    Added akamai to load images

--- pages/sell-nft.js
+++ pages/sell-nft.js
@@ -1,5 +1,3 @@
-import Head from "next/head"
-import Image from "next/image"
 import styles from "../styles/Home.module.css"
 import { Form, Button, useNotification } from "@web3uikit/core"
 import { ethers } from "ethers"
